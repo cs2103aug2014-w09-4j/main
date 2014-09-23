cc5e7fdb2beb13afd3b8678ef23428b3aa509ff1
zxctanner <kerine@kerines-MacBook-Pro.local>
2014-09-23 16:03:06 +0800

    Added status variable

--- Project/Task.java
+++ Project/Task.java
@@ -2,6 +2,7 @@
 public class Task(){
 	private String taskName;
 	private String date;
+	private boolean status;
 	
 	public String getTaskName(){
 		return this.taskName;
@@ -9,5 +10,13 @@ public class Task(){
 
 	public String setTaskName(String taskName){
 		this.taskName = taskName;
+	}
+	
+	public boolean getStatus(){
+		return this.status;
+	}
+
+	public boolean setStatus(boolean status){
+		this.status = status;
 	}
 }
