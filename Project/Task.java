1775fd9a33dedaa811f2a4b17c39d05d7a0c5960
zxctanner <kerine@kerines-MacBook-Pro.local>
2014-09-23 15:51:52 +0800

    Upload Task.java

---
+++ Project/Task.java
@@ -0,0 +1,13 @@
+
+public class Task(){
+	private String taskName;
+	private String date;
+	
+	public String getTaskName(){
+		return this.taskName;
+	}
+
+	public String setTaskName(String taskName){
+		this.taskName = taskName;
+	}
+}
