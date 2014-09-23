011c55b90601cdac782b89a780ffefc8f5792272
Jeremy <jomsenong@hotmail.com>
2014-09-23 16:14:01 +0800

    marking of tasks done
    Boolean status of tasks implemented

--- Project/Logic.java
+++ Project/Logic.java
@@ -8,14 +8,12 @@ public class Logic {
 			Task newtask = new Task();
 			newtask.setTaskName(description);
 			common.task.add(newtask);
-			Writer.store();
 		}
 		else if(command.equals("delete")){
 			String description = common.getDescription();
 			Task deleteTask = new Task();
 			deleteTask.setTaskName(description);
 			common.task.remove(deleteTask);
-			Writer.store();
 		}
 		else if(command.equals("update")){
 			String description = common.getDescription();
@@ -26,7 +24,15 @@ public class Logic {
 			newTask.setTaskName(newDescription);
 			common.task.remove(deleteTask);
 			common.task.add(newTask);
-			Writer.store();
+		}
+		else if(command.equals("done")){
+			String description = common.getDescription();
+			for(int i = 0; i < common.task.size(); i++){
+				String compare = common.task.getTaskName();
+				if(description.equals(compare);
+				common.task.setStatus(true);
+				}
+			}
 		}
 	}
 }
