90741f2f4cede9aa325b839a030b121e7612ba7b
Nicholas Low <niggleslow@gmail.com>
2014-09-29 00:35:13 +0800

    Storage Class Update
    Changing format of data stored in local file into JSON format so that it
    will be human-editable. Added class description.

--- Project/Storage.java
+++ Project/Storage.java
@@ -1,3 +1,33 @@
+/* 
+---------------------------------------------------------------------------------- STORAGE CLASS ----------------------------------------------------------------------------------
+
+This class handles the storing and retrieving of existing tasks from the local file "Tasks.txt".
+
+It has the following operations:
+
+-addTask <addition of tasks to the list of tasks>
+-removeTask <removal of specific task from list of tasks>
+-getTask <retrieve specific task from list of tasks>
+
+-loadTasksFromFile <retrieves list of tasks (JSON format) from local file and converts it into ArrayList<Task> for easy manipulation>
+-saveTasksIntoFile <converts ArrayList<Task> to (JSON format) String and store it in local file "Tasks.txt">
+
+NOTE: changes made to task list will be saved during addTask and removeTask function calls as saveTasksIntoFile is called at end of both operations.
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+
 public class Storage {
 	
 	private ArrayList<Task> tasks;
@@ -29,20 +59,25 @@ public class Storage {
 	}
 	
 	private ArrayList<Task> loadTasksFromFile(File fileName) {
+		Gson gson = new Gson();
 		ArrayList<Task> temp = new ArrayList<Task>();
-		FileInputStream fis = new FileInputStream(fileName);
-		ObjectInputStream ois = new ObjectInputStream(fis);
-        temp = (ArrayList<Task>)ois.readObject();
-        ois.close();
-        fis.close();
-        return temp;
+		String jsonTasks = "";
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+		    jsonTasks = line;
+		}
+		Type type = new TypeToken<ArrayList<Task>>() {
+        	}.getType();
+        ArrayList<Task> taskList = gson.fromJson(jsonTasks, type);
+        return taskList;
 	}
 	
-	private ArrayList<Task> saveTasksIntoFile(File fileName) {
-		FileOutputStream fos = new FileOutputStream(fileName);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(this.tasks);
-		oos.close();
-		fos.close();
+	private void saveTasksIntoFile(File fileName) {
+		Gson gson = new Gson();
+		String jsonTasks = gson.toJson(this.tasks);
+		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+		writer.println(jsonTasks);
+		writer.close();
 	}
 }
