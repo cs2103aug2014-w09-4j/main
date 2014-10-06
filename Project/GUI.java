d2da05c661f6ae134522089651303c121f01c4d9
WangYanHao <wangyanhao1993@163.com>
2014-10-06 14:39:58 +0800

    Refractor GUI, add KeyListener to ScrollPane

--- Project/GUI.java
+++ Project/GUI.java
@@ -32,17 +32,24 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.BoundedRangeModel;
+import javax.swing.InputMap;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.text.JTextComponent;
 
 import com.google.gson.JsonSyntaxException;
 
@@ -55,8 +62,16 @@ import com.google.gson.JsonSyntaxException;
  *
  */
 public class GUI{
-	private final String APP_NAME = "Taskerino";
-	
+	private static final String APP_NAME = "Taskerino";
+    private static final String UP = "Up";
+    private static final String DOWN = "Down";
+    private static final int SCROLLABLE_INCREMENT = 14;
+    
+	private final JPanel panel = new JPanel();
+	private final JTextField userInputArea = new JTextField();
+	private final JLabel outputToUserArea = new JLabel();
+	private final JScrollPane scrollPane = new JScrollPane();
+
 	public GUI() {
 		JFrame frame = new JFrame(APP_NAME);
 
@@ -76,23 +91,15 @@ public class GUI{
 	}
 
 	private Component createComponents(){
-		final JTextField input = new JTextField();
-		final JLabel output = new JLabel();
-		final JScrollPane scrollPane = new JScrollPane();
-
-		JPanel panel = new JPanel();
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
 
-		panel.add(input);
-		panel.add(scrollPane);
-		
 		// add action listener for event when user presses "enter" in keyboard
-		input.addActionListener(new ActionListener() {
+		userInputArea.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				String inputCommand = input.getText();
-				input.setText("");
+			public void actionPerformed(ActionEvent ae) {
+				String inputCommand = userInputArea.getText();
+				userInputArea.setText("");
 				
 				// inputCommand is passed into parser and logic
 				Common.setInput(inputCommand);
@@ -105,11 +112,28 @@ public class GUI{
 					e.printStackTrace();
 				}
 				
-				output.setText(outputFeedBack);
+				outputToUserArea.setText(outputFeedBack);
 			}
 		});
 
-		scrollPane.getViewport().add(output);
+		scrollPane.getViewport().add(outputToUserArea);
+
+        // add key bindings to the JLabel which display feedback to user
+        int condition = JLabel.WHEN_IN_FOCUSED_WINDOW;
+        InputMap inMap = outputToUserArea.getInputMap(condition);
+        ActionMap actMap = outputToUserArea.getActionMap();
+		
+        inMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), UP);
+        inMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), DOWN);
+        
+        actMap.put(UP, new UpDownAction(UP, scrollPane.getVerticalScrollBar().getModel(), 
+        		SCROLLABLE_INCREMENT));
+        actMap.put(DOWN, new UpDownAction(DOWN, scrollPane.getVerticalScrollBar().getModel(), 
+        		SCROLLABLE_INCREMENT));
+        
+		// set layout for JPanel
+		panel.add(userInputArea);
+		panel.add(scrollPane);
 		
 		panel.setFont(new Font("SansSerif", Font.PLAIN, 14));
 		panel.setLayout(layout);
@@ -121,7 +145,7 @@ public class GUI{
 		gbc.weighty = 1.0;
 		gbc.gridheight = 1;
 		gbc.gridwidth = 0;
-		layout.setConstraints(input, gbc);
+		layout.setConstraints(userInputArea, gbc);
 		
 		// set dimension for output label
 		gbc.weightx = 1.0;
@@ -133,6 +157,35 @@ public class GUI{
 		return panel;
 	}
 	
+	
+	// Action performed when user press "up" or "down" on keyboard
+    @SuppressWarnings("serial")
+	private class UpDownAction extends AbstractAction {
+        private BoundedRangeModel vScrollBarModel;
+        private int scrollableIncrement;
+        
+        public UpDownAction(String name, BoundedRangeModel model, int scrollableIncrement) {
+            super(name);
+            this.vScrollBarModel = model;
+            this.scrollableIncrement = scrollableIncrement;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String name = getValue(AbstractAction.NAME).toString();
+            int value = vScrollBarModel.getValue();
+            
+            if (name.equals(UP)) {
+                value -= scrollableIncrement;
+                vScrollBarModel.setValue(value);
+            } else if (name.equals(DOWN)) {
+                value += scrollableIncrement;
+                vScrollBarModel.setValue(value);
+            }
+        }
+    }
+    
+    
 	public static void main(String[] args) throws JsonSyntaxException, IOException {
 		Common.task = Storage.loadTasksFromFile();
 		new GUI();
