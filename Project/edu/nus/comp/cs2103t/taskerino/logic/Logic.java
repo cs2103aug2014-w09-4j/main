6aaf558d737b709b5ca1cf729a40a147af1cd77b
Nicholas Low <niggleslow@gmail.com>
2014-10-21 15:47:30 +0800

    Editted completeTask and searchTask for logic

--- Project/edu/nus/comp/cs2103t/taskerino/logic/Logic.java
+++ Project/edu/nus/comp/cs2103t/taskerino/logic/Logic.java
@@ -2,8 +2,10 @@ package edu.nus.comp.cs2103t.taskerino.logic;
 
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
-import edu.nus.comp.cs2103t.taskerino.common.*;
+import edu.nus.comp.cs2103t.taskerino.common.Data;
+import edu.nus.comp.cs2103t.taskerino.common.Task;
 import edu.nus.comp.cs2103t.taskerino.storage.Storage;
 
 
@@ -43,6 +45,7 @@ public class Logic {
 		}
 			
 	}
+	
 	/**
 	 * ChangeTask function replaces the exiting task details
 	 * with new task details
@@ -53,12 +56,14 @@ public class Logic {
 		String newDescription = Data.getNewDescription();
 		
 		try {
-			Storage.updateTask(description, newDescription);
+			Data.updateTask(description, newDescription);
+			Storage.saveTasksIntoFile();
 			return "update task successfully from " + description + " to " + newDescription;
 		} catch (ArrayIndexOutOfBoundsException e) {
 			return "Task with name: " + description + " not found!";
 		}
 	}
+	
 	/**
      * deleteTask removes the task from the existing tasks list 
      * and return a String feedback to GUI class.
@@ -66,12 +71,14 @@ public class Logic {
 	private String deleteTask() throws FileNotFoundException, UnsupportedEncodingException {
 		String description = Data.getDescription();
 		try {
-			Storage.removeTask(description);
+			Data.removeTask(description);
+			Storage.saveTasksIntoFile();
 			return "delete task " + description + " successfully";
 		} catch (ArrayIndexOutOfBoundsException e) {
 			return "Task with name: " + description + " not found!";
 		}
 	}
+	
 	/**
      * addTask adds the task into the existing tasks list 
      * and return a String feedback to GUI class.
@@ -79,15 +86,56 @@ public class Logic {
 	private String addTask() throws FileNotFoundException, UnsupportedEncodingException {
 		String description = Data.getDescription();
 		Task newTask = new Task();
-		
 		newTask.setTaskName(description);
-		Storage.addTask(newTask);
-		
+		Data.addTask(newTask);
+		Storage.saveTasksIntoFile();
 		// dummy
 		return "add task " + newTask.getTaskName() + " successfully";
 	}
 	
-	public String testCommand(String userCommand) {
+	/**
+     * completeTask marks the task from the existing tasks list 
+     * and return a String feedback to GUI class.
+     */
+	private String completeTask() throws FileNotFoundException, UnsupportedEncodingException {
+		String description = Data.getDescription();
+		Task toBeCompleted = Data.getTask(description);
+		if(toBeCompleted != null) {
+			toBeCompleted.setStatus(true);
+			Storage.saveTasksIntoFile();
+			return "complete task " + description;
+		} else {
+			return "Task with name: " + description + " not found!";
+		}
+	}
+	
+	/**
+     * searchTask search the existing tasks list 
+     * obtain the tasks from the existing tasks list
+	 * and return the details of the tasks to GUI class
+     */
+	
+	private String searchTask() {
+		String description = Data.getDescription();
+		String tasksFound = "";
+		ArrayList<String> searches = new ArrayList<String>();
+		for(int i = 0; i < Data.task.size(); i++){
+			String details = Data.task.get(i).getTaskName();
+			String[] parts = details.split(" ");
+			for(int j = 0; j < parts.length; j++){
+				if(parts[j].equals(description)){
+					searches.add(details);
+					break;
+				}
+			}
+		}
+		for(int a = 0; a < searches.size(); a++) {
+			tasksFound += a+1 +". " + searches.get(a) + "\n";
+		}
+		return tasksFound;
+	}
+	
+	public String testCommand(String userCommand) throws FileNotFoundException, UnsupportedEncodingException {
 		switch (userCommand) {
 			case COMMAND_ADD:
 				return addTask();
@@ -106,40 +154,5 @@ public class Logic {
 				return "invalid msg";
 		}
 	}
-	/**
-     * completeTask marks the task from the existing tasks list 
-     * and return a String feedback to GUI class.
-     */
-	/*private String completeTask() {
-		String description = Data.getDescription();
-		for(int i = 0; i < Data.task.size(); i++){
-			if(Data.task.get(i).getTaskName().equals(description)){
-				Data.task.get(i).setStatus(true);
-				return "complete task " + description;
-			}
-		}
-		if(i == Data.task.size()){
-			return "Task with name: " description + " not found!";
-		}
-	}*/
-	/**
-     * searchTask search the existing tasks list 
-     * obtain the tasks from the exisint tasks list
-	 * and return the details of the tasks to GUI class
-     */
-	/*private ArrayList<String> searchTask() {
-		String description = Data.getDescription();
-		ArrayList<String> searches = new ArrayList<String>();
-		for(int i = 0; i < Data.task.size(); i++){
-			String details = Data.task.get(i).getTaskName();
-			String[] parts = details.split(" ");
-			for(int j = 0; j < parts.size(); j++){
-				if(parts[j].equals(description)){
-					searches.add(details);
-					break;
-				}
-			}
-		}
-		return searches;
-	}
 }
+	
