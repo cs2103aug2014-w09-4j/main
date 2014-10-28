d05520a2d7a316d2469a0c94e342da26255db8e1
Nicholas Low <niggleslow@gmail.com>
2014-10-28 20:44:56 +0800

    Fixed logic tests

--- Project/edu/nus/comp/cs2103t/taskerino/logic/LogicTest.java
+++ Project/edu/nus/comp/cs2103t/taskerino/logic/LogicTest.java
@@ -1,25 +1,30 @@
 package edu.nus.comp.cs2103t.taskerino.logic;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 import edu.nus.comp.cs2103t.taskerino.common.Data;
+import edu.nus.comp.cs2103t.taskerino.common.Task;
+
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 public class LogicTest {
 
 	@Test
 	public void addFunction() throws FileNotFoundException, UnsupportedEncodingException{
-		Data data = new Data();
+		Data.setAddType("floating");
+		Data.setDescription("do tutorial");
+		Data.task = new ArrayList<Task>();
 		Logic logic = new Logic();
-		data.setAddType("floating");
-		data.setDescription("do tutorial");
 		assertEquals("add task do tutorial successfully", logic.addTask());
 	}
 	
 	@Test
 	public void deleteFunction() throws FileNotFoundException, UnsupportedEncodingException{
-		Data data = new Data();
-		data.setDescription("do tutorial");
-		data.setAddType("floating");
+		Data.setDescription("do tutorial");
+		Data.setAddType("floating");
+		Data.task = new ArrayList<Task>();
 		Logic logic = new Logic();
 		logic.addTask();
 		assertEquals("delete task do tutorial successfully", logic.deleteTask());
@@ -28,26 +33,26 @@ public class LogicTest {
 	
 	@Test
 	public void changeFunction() throws FileNotFoundException, UnsupportedEncodingException{
-		Data data = new Data();
 		Logic logic = new Logic();
-		data.setDescription("help");
-		data.setAddType("floating");
+		Data.setDescription("help");
+		Data.setAddType("floating");
+		Data.task = new ArrayList<Task>();
 		logic.addTask();
-		data.setNewDescription("no help");
-		assertEquals("Update task from help to no help", logic.changeTask());
+		Data.setNewDescription("no help");
+		assertEquals("update task successfully from help to no help", logic.changeTask());
 		assertEquals("Task with name: help not found!", logic.changeTask());
 	}
 	
 	@Test
 	public void searchFunction() throws FileNotFoundException, UnsupportedEncodingException{
-		Data data = new Data();
 		Logic logic = new Logic();
-		data.setDescription("an apple");
-		data.setAddType("floating");
+		Data.setDescription("an apple");
+		Data.setAddType("floating");
+		Data.task = new ArrayList<Task>();
 		logic.addTask();
 		Data.setDescription("apple");
-		assertEquals("1. an apple", logic.searchTask());
-		data.setDescription("pear");
-		assertEquals("Task with name : pear not found!", logic.searchTask());
+		assertEquals("1. an apple \n", logic.searchTask());
+		Data.setDescription("pear");
+		assertEquals("Task with name: pear not found!", logic.searchTask());
 	}
 }
