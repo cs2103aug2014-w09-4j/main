5582930a02ec7df35982c3871fb101113819b3be
WangYanHao <wangyanhao1993@163.com>
2014-11-04 19:47:26 +0800

    Improved System Testing checking local file validity.

--- Project/edu/nus/comp/cs2103t/taskerino/common/SystemTest.java
+++ Project/edu/nus/comp/cs2103t/taskerino/common/SystemTest.java
@@ -26,7 +26,17 @@
  */
 package edu.nus.comp.cs2103t.taskerino.common;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 import org.junit.Test;
 
@@ -37,75 +47,118 @@ import org.junit.Test;
  *
  */
 public class SystemTest {
+	private Controller controller;
+	private File taskFile;
+	private File backupFile;
+	private File testingFile;
+	private String expectedOutput = "";
+	private String actualOutput = "";
+
+	// format of storage into Gson
+	private static final String QUOTATION_MARK = "\\u0027";
+	private static final String DATE_CLASS_PATH = (new DateAndTime()).getClass().getName();
+	private static final String FLOATING_TASK_FORMAT = "[{\"taskName\":\"%1$s\"," + "\"status\":%2$s}]";
+	private static final String DEADLINE_TASK_FORMAT = "[{\"taskName\":\"%1$s\"," 
+			+ "\"dueDate\":{\"className\":\"" + DATE_CLASS_PATH + "\",\"year\":%2$s,\"month\":%3$s,\"day\":%4$s}," 
+			+ "\"taskType\":\"deadline\"," + "\"status\":%5$s}]";
+	private static final String TIMED_TASK_FORMAT = "[{\"taskName\":\"%1$s\"," 
+			+ "\"dueDate\":{\"className\":\"" + DATE_CLASS_PATH + "\",\"year\":%2$s,\"month\":%3$s,\"day\":%4$s}," 
+			+ "\"dueDate\":{\"className\":\"" + DATE_CLASS_PATH + "\",\"year\":%5$s,\"month\":%6$s,\"day\":%7$s}," 
+			+ "\"taskType\":\"timed\"," + "\"status\":%8$s}]";
+	private static final String EMPTY_TASK_FORMAT = "[]";
+
 
 	@Test
 	public void testBasicTaskCommands() {
-		// set up
-		Controller.loadData();
-		Controller controller = Controller.getController();
-		String expectedOutput = "";
-		String actualOutput = "";
-
+		setUp();
 
 		// test for adding floating task
 		controller.executeUserCommand("add do tutorial");
+		// check for feedback
 		expectedOutput = "Add task do tutorial successfully";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+		// check for file
+		String[] expectedData01 = {"floating", "do tutorial", "false"};
+		writeTestFile(expectedData01);
+		assertEquals(true, fileCompare(taskFile, testingFile));
+
 
 		// test for deleting floating task
 		controller.executeUserCommand("delete do tutorial");
+		// check for feedback
 		expectedOutput = "Delete task do tutorial successfully";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+		// check for file
+		String[] expectedData02 = {"empty"};
+		writeTestFile(expectedData02);
+		assertEquals(true, fileCompare(taskFile, testingFile));
+
 
 		// test for deleting none-existent task
 		controller.executeUserCommand("delete do tutorial");
+		// check for feedback
 		expectedOutput = "Task with name: do tutorial not found!";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+		// check for file
+		String[] expectedData03 = {"empty"};
+		writeTestFile(expectedData03);
+		assertEquals(true, fileCompare(taskFile, testingFile));
+
 
 		// test for changing existing task's description
 		controller.executeUserCommand("add go to lecture");
 		controller.executeUserCommand("change go to lecture to~ don't go to lecture");
+		// check for feedback
 		expectedOutput = "Update task successfully from go to lecture to don't go to lecture";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+		// check for file 
+		String[] expectedData04 = {"floating", "don" + QUOTATION_MARK + "t go to lecture", "false"};
+		writeTestFile(expectedData04);
+		assertEquals(true, fileCompare(taskFile, testingFile));
+
 
 		// test for changing none-existent task's description
 		controller.executeUserCommand("change test case 1 to~ hey!");
+		// check for feedback
 		expectedOutput = "Task with name: test case 1 not found!";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
 
+
 		// delete test case to make local file clean
 		controller.executeUserCommand("delete don't go to lecture");
+		// check for feedback
 		expectedOutput = "Delete task don't go to lecture successfully";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+
+		closeDown();
 	}
 
 
 	@Test
 	public void testTaskWithDateCommands() {
-		// set up
-		Controller.loadData();
-		Controller controller = Controller.getController();
-		String expectedOutput = "";
-		String actualOutput = "";
+		setUp();
 
 		// test for adding timed task
-		controller.executeUserCommand("add test from~ 29 10 2014 to~ 30 10 2014");
+		controller.executeUserCommand("add test from~ 29 10 2014 to~ 30 10 2014");	
+		// check for feedback
 		expectedOutput = "Add task test successfully";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+
+
 
 		// test for changing start date for timed task
 		controller.executeUserCommand("change start date to~ 30 11 2014 from~ test");
 		expectedOutput = "Updated test start time successfully to 30/11/2014";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
-		
+
 		// test for changing start date for timed task for different input month format
 		controller.executeUserCommand("change start date to~ 12 December 2014 from~ test");
 		expectedOutput = "Updated test start time successfully to 12/12/2014";
@@ -117,11 +170,177 @@ public class SystemTest {
 		expectedOutput = "Updated test end time successfully to 1/1/2014";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
-		
+
 		// delete test case to make local file clean
 		controller.executeUserCommand("delete test");
 		expectedOutput = "Delete task test successfully";
 		actualOutput = controller.getUserFeedback();
 		assertEquals(expectedOutput, actualOutput);
+
+		closeDown();
 	}
+
+
+	/**
+	 * Based on Data's type, format the expected output result, and write it into the testingFile.
+	 * @param expectedData String of Data's attributes.
+	 */
+	private void writeTestFile(String[] expectedData) {
+		PrintWriter printer = null;
+		try {
+			printer = new PrintWriter(new FileOutputStream(testingFile), true);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		switch (expectedData[0]) {
+		case "floating":
+			printer.printf(FLOATING_TASK_FORMAT, expectedData[1], expectedData[2]);
+			printer.println("");
+			break;
+		case "deadline":
+			printer.printf(DEADLINE_TASK_FORMAT, expectedData[1], expectedData[2], expectedData[3],
+					expectedData[4], expectedData[5]);
+			printer.println("");
+			break;
+		case "timed":
+			printer.printf(TIMED_TASK_FORMAT, expectedData[1], expectedData[2], expectedData[3],
+					expectedData[4], expectedData[5], expectedData[6], expectedData[7], expectedData[8]);
+			printer.println("");
+			break;
+		case "empty":
+			printer.println(EMPTY_TASK_FORMAT);
+			break;
+		}
+		printer.close();
+	}
+
+
+	/**
+	 * Close down system testing, making sure local directory is clean: <br>
+	 * 1. Delete and re-create task file to make sure it is clean. <br>
+	 * 1. Copy backup file to original task file. <br>
+	 * 2. Delete testing file and backup file. <br>
+	 */
+	private void closeDown() {
+		taskFile.delete();
+		taskFile = new File("Tasks.txt");
+
+		try {
+			copyFile(backupFile, taskFile);		
+			backupFile.delete();
+			testingFile.delete();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * Compare two local files byte by byte, check if they have the same content.
+	 * @param sourceFile
+	 * @param destFile
+	 * @return boolean indicating if two files are the same.
+	 * @throws IOException file too large to compare
+	 */
+	private boolean fileCompare(File sourceFile, File destFile) {
+		try {
+			InputStream srcIs = new FileInputStream(sourceFile);
+			BufferedInputStream srcBis = new BufferedInputStream(srcIs);
+			final byte[] srcByte = new byte[1024];
+
+			InputStream destIs = new FileInputStream(destFile);
+			BufferedInputStream destBis = new BufferedInputStream(destIs);
+			final byte[] destByte = new byte[1024];
+
+			int srcCount = srcBis.read(srcByte);
+			int destCount = destBis.read(destByte);
+
+			do {
+				// compare
+				if (srcCount != destCount) {
+					srcBis.close();
+					destBis.close();
+					return false;
+				}
+				for(int i = 0; i < srcCount; i++) {
+					if(srcByte[i] != destByte[i]) {
+						srcBis.close();
+						destBis.close();
+						return false;
+					}
+				}
+
+				srcCount = srcBis.read(srcByte);
+				destCount = destBis.read(destByte);
+
+			} while (srcCount > 0 && destCount > 0);
+
+			srcBis.close();
+			destBis.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+
+	/**
+	 * Set up file comparison necessities: <br>
+	 * 1. Load local file. <br>
+	 * 2. Initialize Controller. <br>
+	 * 3. Make copy for local file (ensure local data is not dirty after testing). <br>
+	 * 4. Clear the local file by deleting and recreating it. <br>
+	 * 5. Create testing file.
+	 */
+	private void setUp() {
+		Controller.loadData();
+		controller = Controller.getController();
+
+		taskFile = new File("Tasks.txt");
+		backupFile = new File("SystemTestingBackUp.txt"); 
+
+		try {
+			copyFile(taskFile, backupFile);
+		} catch (IOException e) {
+			// file not found exception
+			e.printStackTrace();
+		}
+
+		taskFile.delete();
+		taskFile = new File("Tasks.txt");
+		Controller.loadData();
+
+		testingFile = new File("SystemTesting.txt");
+	}
+
+
+	/**
+	 * Make copy of a local file.
+	 * @param sourceFile
+	 * @param destFile
+	 * @throws IOException
+	 */
+	private void copyFile(File sourceFile, File destFile) throws IOException {
+		InputStream inputStream = new FileInputStream(sourceFile);
+		BufferedInputStream bis = new BufferedInputStream(inputStream);
+		final byte[] bytes = new byte[1024];
+
+		FileOutputStream outputStream = new FileOutputStream(destFile);
+		BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+
+		int count = bis.read(bytes);
+		while (count > 0) {
+			bos.write(bytes, 0, count);
+			count = bis.read(bytes);
+		}
+
+		bos.close();
+		bis.close();
+
+		inputStream.close();
+		outputStream.close();
+	}
+
 }
