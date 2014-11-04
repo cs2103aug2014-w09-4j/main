327dea239943420afda740fa970492d2e01019de
Jeremy <jomsenong@hotmail.com>
2014-11-05 01:16:54 +0800

    Mark as author

--- Project/edu/nus/comp/cs2103t/taskerino/common/DataTest.java
+++ Project/edu/nus/comp/cs2103t/taskerino/common/DataTest.java
@@ -1,9 +1,6 @@
 //@author A0110574N
 package edu.nus.comp.cs2103t.taskerino.common;
- /**
-  * @author A0110574N
-  * 
-  */
+
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
